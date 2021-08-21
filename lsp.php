94fa47b3aa98935d71dcd02a0ca494da58e46468
user <test@test.test>
2021-08-21 20:31:29 +0300

    Lsp

---
+++ lsp.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Solid\Examples\LSP;
+
+class BaseGenerator
+{
+    public function generate($value)
+    {
+        return $value;
+    }
+}
+
+class ChildGenerator1 extends BaseGenerator
+{
+    public function generate($value)
+    {
+        //Input value- additional condition for input data
+        if ($value < 0) {
+            throw new \Exception('Incorrect value');
+        }
+        return $value;
+    }
+}
+
+class ChildGenerator2 extends BaseGenerator
+{
+    public function generate($value, $value2 = null)
+    {
+        //Additional return type
+        if ($value < 0) {
+            $value = null;
+        }
+        return $value;
+    }
+}
+
+class ChildGenerator3 extends BaseGenerator
+{
+    public function generate($value)
+    {
+        //Changed return type
+        return [$value];
+    }
+}
+
+class Main
+{
+    public function handle(BaseGenerator $generator): int
+    {
+        //Expect integer as a result
+        return $generator->generate(-2);
+    }
+}
+
+$main = new Main();
+$base = new BaseGenerator();
+$value = $main->handle($base);
+var_dump($value);
+//int(-2)
+
+$child = new ChildGenerator1();
+$value = $main->handle($child);
+var_dump($value);
+//Fatal error: Uncaught Exception: Incorrect value in lsp.php:19 Stack trace: #0 lsp.php(50): Solid\Examples\LSP\ChildGenerator1->generate() #1 lsp.php(60): Solid\Examples\LSP\Main->handle() #2 {main} thrown in lsp.php on line 19
+
+$child = new ChildGenerator2();
+$value = $main->handle($child);
+var_dump($value);
+//Fatal error: Uncaught TypeError: Solid\Examples\LSP\Main::handle(): Return value must be of type int, null returned in lsp.php:50 Stack trace: #0 lsp.php(65): Solid\Examples\LSP\Main->handle() #1 {main} thrown in lsp.php on line 50
+
+$child = new ChildGenerator3();
+$value = $main->handle($child);
+var_dump($value);
+//Fatal error: Uncaught TypeError: Solid\Examples\LSP\Main::handle(): Return value must be of type int, array returned in lsp.php:50 Stack trace: #0 lsp.php(70): Solid\Examples\LSP\Main->handle() #1 {main} thrown in lsp.php on line 50
+
+
+
+
+
+
+
