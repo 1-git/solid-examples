bf7367d17655e8f54eb2f2df6f73f6ffab30620c
1-git <one-git@yandex.com>
2021-08-23 22:53:52 +0300

    Isp

--- isp.php
+++ isp.php
@@ -2,3 +2,29 @@
 
 namespace Solid\Examples\ISP;
 
+interface InvoiceInterface
+{
+    public function getItems();
+    public function getTotalPrice();
+    public function getAddress();
+
+    public function showHeader();
+    public function showContent();
+    public function showFooter();
+}
+
+//Separated interfaces
+
+interface InvoiceDataInterface
+{
+    public function getItems();
+    public function getTotalPrice();
+    public function getAddress();
+}
+
+interface InvoiceInvoiceViewerInterface
+{
+    public function showHeader();
+    public function showContent();
+    public function showFooter();
+}
