f51650519e0f21e21ea2268d6f1e15292bcff694
1-git <one-git@yandex.com>
2021-09-01 16:37:44 +0300

    Ocp

--- ocp.php
+++ ocp.php
@@ -2,6 +2,45 @@
 
 namespace Solid\Examples\OCP;
 
-class Auto {
+###############################################
+## PROBLEM 1 - DIFFICULT TO ADD FUNCTIONALITY #
+###############################################
 
+class Image {
+    public function SaveToFile(string $path)
+    {
+    }
+}
+
+class ImageWrongModified {
+    public function SaveToBmp(string $path)
+    {
+    }
+    public function SaveToJpg(string $path)
+    {
+    }
+    public function SaveToPng(string $path)
+    {
+    }
+}
+
+###############################################
+################## SOLUTION ###################
+###############################################
+
+interface SaveImageInterface {
+    public function save(string $path);
+}
+
+class  SaveBpm {
+    public function save(string $path){
+    }
+}
+class  SaveJpg {
+    public function save(string $path){
+    }
+}
+class  SavePng {
+    public function save(string $path){
+    }
 }
